828e8078f98cd1a38bbc640d3f0109a0f89549cd
matt handler <matt.handler@gmail.com>
2014-12-03 23:00:35 -0500

    update spec

--- spec/imdb/movie_spec.rb
+++ spec/imdb/movie_spec.rb
@@ -16,53 +16,22 @@ describe 'Imdb::Movie' do
       @movie = Imdb::Movie.new('0095016')
     end
 
-    it 'should find the cast members' do
-      cast = @movie.cast_members
+    describe ".cast" do
 
-      cast.should be_an(Array)
-      cast.should include('Bruce Willis')
-      cast.should include('Bonnie Bedelia')
-      cast.should include('Alan Rickman')
-    end
-
-    it 'should find the cast characters' do
-      char = @movie.cast_characters
-
-      char.should be_an(Array)
-      char.should include('Karl')
-      char.should include('Officer John McClane')
-      char.should include('Police Detective (uncredited)')
-      char.should include('Hostage')
-    end
-
-    it 'should associates the cast members to the characters' do
-      cast = @movie.cast_members
-      char = @movie.cast_characters
-      cast_char = @movie.cast_members_characters
-
-      cast_char[0].should eql("#{cast[0]} => #{char[0]}")
-      cast_char[10].should eql("#{cast[10]} => #{char[10]}")
-      cast_char[-1].should eql("#{cast[-1]} => #{char[-1]}")
-
-      cast_char = @movie.cast_members_characters('as')
-
-      cast_char[1].should eql("#{cast[1]} as #{char[1]}")
-      cast_char[11].should eql("#{cast[11]} as #{char[11]}")
-      cast_char[-2].should eql("#{cast[-2]} as #{char[-2]}")
-    end
-
-    describe 'fetching a list of imdb actor ids for the cast members' do
-      it 'should not require arguments' do
-        expect { @movie.cast_member_ids }.not_to raise_error
+      before(:each) do
+        @cast = @movie.cast
       end
 
-      it 'should not allow arguments' do
-        expect { @movie.cast_member_ids(:foo) }.to raise_error(ArgumentError)
+      it 'returns a list of people' do
+        @cast.first.should be_an(Imdb::Person)
       end
 
-      it 'should return the imdb actor number for each cast member' do
-        @movie.cast_member_ids.sort.should == %w(nm0000246 nm0000614 nm0000889 nm0000952 nm0001108 nm0001817 nm0005598 nm0033749 nm0040472 nm0048326 nm0072054 nm0094770 nm0101088 nm0112505 nm0112779 nm0119594 nm0127960 nm0142420 nm0160690 nm0162041 nm0234426 nm0236525 nm0239958 nm0278010 nm0296791 nm0319739 nm0322339 nm0324231 nm0326276 nm0338808 nm0356114 nm0370729 nm0383487 nm0416429 nm0421114 nm0441665 nm0484360 nm0484650 nm0493493 nm0502959 nm0503610 nm0504342 nm0539639 nm0546076 nm0546747 nm0662568 nm0669625 nm0681604 nm0687270 nm0688235 nm0718021 nm0731114 nm0776208 nm0793363 nm0852311 nm0870729 nm0882139 nm0902455 nm0907234 nm0924636 nm0936591 nm0958105 nm2143912 nm2476262 nm2565888).sort
+      it 'finds cast members' do
+        @cast.find{|p| p.name == 'Bruce Willis'}.should_not be_nil
+        @cast.find{|p| p.name == 'Bonnie Bedelia'}.should_not be_nil
+        @cast.find{|p| p.name == 'Alan Rickman'}.should_not be_nil
       end
+
     end
 
     it 'returns the url to the movie trailer' do
@@ -73,7 +42,7 @@ describe 'Imdb::Movie' do
     it 'should find the director' do
       @movie.director.should be_an(Array)
       @movie.director.size.should eql(1)
-      @movie.director.first.should =~ /John McTiernan/
+      @movie.director.first.name.should =~ /John McTiernan/
     end
 
     it 'should find the company info' do
@@ -118,7 +87,7 @@ describe 'Imdb::Movie' do
     end
 
     it 'should find plot synopsis' do
-      @movie.plot_synopsis.should =~ /John McClane, a detective with the New York City Police Department, arrives in Los Angeles to attempt a Christmas reunion and reconciliation with his estranged wife Holly, who is attending a party thrown by her employer, the Nakatomi Corporation at its still-unfinished American branch office headquarters, the high-rise Nakatomi Plaza. When McClane refreshes himself from the flight in Holly's corporate room, they have an argument over the use of her maiden name, Gennero, but Holly is called away/
+      @movie.plot_synopsis.should include('John McClane, a detective with the New York City Police Department')
     end
 
     it 'should find plot summary' do
@@ -134,7 +103,7 @@ describe 'Imdb::Movie' do
     end
 
     it 'should find number of votes' do
-      @movie.votes.should be_within(10_000).of(420_900)
+      @movie.votes.should be_within(10_000).of(475274)
     end
 
     it 'should find the title' do
@@ -157,8 +126,8 @@ describe 'Imdb::Movie' do
 
         movie.director.should be_an(Array)
         movie.director.size.should eql(2)
-        movie.director.should include('Lana Wachowski')
-        movie.director.should include('Andy Wachowski')
+        movie.director.find{|d| d.name == 'Lana Wachowski' }.should_not be_nil
+        movie.director.find{|d| d.name == 'Andy Wachowski' }.should_not be_nil
       end
 
       it 'should find writers' do
@@ -181,7 +150,7 @@ describe 'Imdb::Movie' do
     it "should find multiple 'also known as' versions" do
       also_known_as = @movie.also_known_as
       also_known_as.should be_an(Array)
-      also_known_as.size.should eql(40)
+      also_known_as.size.should be > 30
     end
 
     it "should find a specific 'also known as' version" do
@@ -210,7 +179,7 @@ describe 'Imdb::Movie' do
 
     it "should not have a 'more' link in the plot" do
       movie = Imdb::Movie.new('0036855')
-      movie.plot.should eql('Years after her aunt was murdered in her home, a young woman moves back into the house with her new husband. However, he has a secret which he will do anything to protect, even if that means driving his wife insane.')
+      movie.plot.should include('Years after her aunt was murdered in her home')
     end
   end
 
